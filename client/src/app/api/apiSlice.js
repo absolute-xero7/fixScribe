b8dcce7bf03263563f3ab732c8e885eb6d3700f2
absolute_xero7 <prahlad.ranjit7@gmail.com>
2024-06-10 03:27:32 -0400

    added correct backend url to frontend

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from '../../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://fixScribe-api.onrender.com',
+    baseUrl: 'https://fixscribe-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
